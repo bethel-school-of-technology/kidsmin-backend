4d78fe5fe1e6181f2715264cb06bd6d306bb175f
Elijah Smith <elijah.smith1029@gmail.com>
2020-06-10 19:46:35 -0400

    started working with bcrypt in the users.js file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt'); 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.users.findAll().then(users =>{
+    res.json(users)
+  })
 });
 
+app.post('/', (req, res) => {
+const user = { name: req.body.userName, password: req.body.password }
+users.push(user)
+res.status(201).send()
+})
 module.exports = router;
